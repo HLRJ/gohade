61919323eb47e870fb13dc5ae7aefc6cc4a7866c
Zhenxiang Wang <befun@vip.qq.com>
2022-07-05 22:39:56 +0800

    feat: add group.go

---
+++ framework/group.go
@@ -0,0 +1,13 @@
+package framework
+
+// IGroup 代表前缀分组
+type IGroup interface {
+	// 实现HttpMethod方法
+	Get(string, ControllerHandler)
+	Post(string, ControllerHandler)
+	Put(string, ControllerHandler)
+	Delete(string, ControllerHandler)
+
+	// 实现嵌套group
+	Group(string) IGroup
+}
