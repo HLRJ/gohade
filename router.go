391b0bd2eccf5aa276f37907b245bb9912a9ce21
Zhenxiang Wang <befun@vip.qq.com>
2022-07-05 22:42:03 +0800

    chore: add complex router

--- router.go
+++ router.go
@@ -4,7 +4,14 @@ import (
 	"gohade/framework"
 )
 
+//注册路由规则
 func registerRouter(core *framework.Core) {
 	// core.Get("foo", framework.TimeoutHandler(FooControllerHandler, time.Second*1))
-	core.Get("foo", FooControllerHandler)
+	//需求 HTTP方法+静态路由匹配
+	core.Get("/user/login", UserLoginController)
+	//需求3：批量通用前缀
+	subjectApi := core.Group("/subject")
+	{
+		subjectApi.Get("/list", SubjectListController)
+	}
 }
